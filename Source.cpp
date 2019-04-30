f15228357981f591c748d19da98942f37f97fe7b
穆鸿翔 <mutou.leo.wood@gmail.com>
2019-04-30 23:30:48 +0800

    还差 dec 和 外围

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,46 @@
 #include<iostream>
 #include<random>
 #include<vector>
+#include<math.h>
+#include"matrix.h"
 using namespace std;
 
-template<int m,int n>
-class matrix
-{
-public:
-	matrix();
-	
-private:
-	int mod;
-	vector<vector<int> > v(m, vector<int>(n));
-};
 
-template<int m, int n>
-matrix<m, n>::matrix()
+void oneRound(int a, int m, int n, int l, int t, int r, int q)
 {
 
 }
 
+pair< pair<matrix, matrix>, matrix> gen(int a, int m, int n, int l, int q)
+{
+	matrix A(m, n, q, UNIFORM), S(n, l, q, UNIFORM), E(m, l, q, GAUSS, (double)a*q / (M_PI * M_SQRT2));
+	matrix B = A * S + E;
+	return pair< pair<matrix, matrix>, matrix>(pair<matrix, matrix>(A, B), S);
+}
+
+pair< matrix, matrix> enc(pair<matrix, matrix>pub, int m, int r, int t, int l, int q)
+{
+	matrix v(l, 1, t, UNIFORM), R(m, 1, 2 * r + 1,UNIFORM);
+	for (int i = 0; i < m; i++)
+	{
+		R[i][0] -= 1;
+	}
+	matrix u = (~(pub.first))*R, c = (~(pub.second))*R + f(v, q);
+	return pair<matrix, matrix>(u, c);
+}
+
+
+
 int main(void)
 {
-	double mu{ 50.0 }, sigma{ 10.0 };
+	/*double mu{ 50.0 }, sigma{ 10.0 };
 	normal_distribution<> norm{ mu, sigma };
 	random_device rd;
 	default_random_engine rng{ rd() };
 	for (int i = 0; i < 1000; i++)
 	{
 		cout << round(norm(rng)) << endl;
-	}
-	//cout << "Normally distributed values: " << norm(rng) << " " << norm(rng) << std::endl;
+	}*/
+	int t = 2;
 	return 0;
 }
-
-
