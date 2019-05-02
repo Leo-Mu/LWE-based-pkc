3e50c0b72ead803352adbf1ebc740e762754ef0f
SCBeta2718 <47864007+SCBeta2718@users.noreply.github.com>
2019-05-02 21:48:19 +0800

    output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <random>
 
 #include "matrix.h"
@@ -11,10 +12,61 @@ const double alpha=0.0024;
 
 //amnltrq
 
+ofstream pl("plaintext.txt");
+ofstream ci("ciphertext.txt");
+ofstream rep("report.txt");
+
+void output(matrix m)
+{
+	for(int i=0;i<m.n;i++)
+	{
+		for(int j=0;j<m.m;j++)
+		{
+			rep<<m[i][j]<<" ";
+		}
+		rep<<"\n";
+	}
+}
+
 int main(void)
 {
-	ofstream pl("plaintext.txt");
-	ofstream ci("ciphertext.txt");
+	
+
+	int t;
+	
+	crypto cpt(alpha,m,n,l,t,r,q);
+
+	cout<<"Please input the number of test cases.";
+	cin>>t;
+
+	pair<matrix,matrix> pub;
+	matrix pri;
+	
+	pub=cpt.pub();
+	pri=cpt.pri();
+
+	rep<<"REPORT:\n";
+	rep<<"\nA:\n";
+	output(pub.first);
+	rep<<"\nS:\n";
+	output(pri);
+	rep<<"\nB:\n";
+	output(pub.second);
+	rep<<"a: "<<a<<"\n";
+	rep<<"m: "<<m<<"\n";
+	rep<<"n: "<<n<<"\n";
+	rep<<"l: "<<l<<"\n";
+	rep<<"t: "<<t<<"\n";
+	rep<<"r: "<<r<<"\n";
+	rep<<"q: "<<q<<"\n";
+
+
+	for(int i=0;i<t;i++)
+	{
+		matrix plt(l);
+
+
+	}
 
 	return 0;
 }
